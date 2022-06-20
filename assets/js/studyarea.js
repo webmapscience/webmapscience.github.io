3234bc8e87e839b011b0e7ce64f3b52ae77cc0ba
ibkrob <102308006+ibkrob@users.noreply.github.com>
2022-06-20 22:28:36 +0200

    rainviewer plugin hinzu

--- assets/js/studyarea.js
+++ assets/js/studyarea.js
@@ -121,6 +121,19 @@ let popup = `
 
 </ul>
 `;
+
+// Rainviewer
+L.control.rainviewer({
+    position: 'bottomleft',
+    nextButtonText: '>',
+    playStopButtonText: 'Play/Stop',
+    prevButtonText: '<',
+    positionSliderLabelText: "Hour:",
+    opacitySliderLabelText: "Opacity:",
+    animationInterval: 500,
+    opacity: 0.5
+}).addTo(map);
+
 gpxLayer.bindPopup(popup);
 });
 
