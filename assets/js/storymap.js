f28f048212741357f85e7abb577bd8a415d50e2c
ibkrob <102308006+ibkrob@users.noreply.github.com>
2022-06-26 17:39:00 +0200

    Karten-Ausschnitt ändern

--- assets/js/storymap.js
+++ assets/js/storymap.js
@@ -9,11 +9,11 @@
             breakpointPos: '33.333%',
             createMap: function () {
                 // create a map in the "map" div, set the view to a given place and zoom
-                var map = L.map('map').setView([65, 18], 5);
+                var map = L.map('map').setView([46.7956981,10.7411067,], 13);
 
                 // add an OpenStreetMap tile layer
                 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    attribution: 'Sussbauer/Wagner <a href="https://webmapscience.github.io">WebmapScience</a>, &copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
                 }).addTo(map);
 
                 return map;
